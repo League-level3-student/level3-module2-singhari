e69e620962873baffe605a25060707de2fd92bab
league <catfish5701@gmail.com>
2024-08-07 20:02:30 -0700

    finished as far I care

--- src/_03_More_Algorithms/Algorithms.java
+++ src/_03_More_Algorithms/Algorithms.java
@@ -8,10 +8,72 @@ import java.util.List;
 public class Algorithms {
     
     public static String multiply(int num1, int num2) {
-        return null;
+        return num1+" x "+num2+" = "+(num1*num2);
+    }
+    public static Boolean isPrime(int n) {
+    	boolean prime = true;
+    	for(int i = 2; i< n; i++) {
+    		if(n%i ==0) {
+    			prime = false;
+    		}
+    	}
+    	
+    	
+    	
+    	return prime;
+    }
+    
+    public static Boolean isSquare(int n) {
+    	boolean square = false;
+    	String check = Math.pow(n, 0.5) + "";
+    	if(check.endsWith(".0")) {
+    		square = true;
+    	}
+    	
+    	return square;
+    }
+    
+    public static Boolean isCube(int n) {
+    	boolean cube = false;
+    	for(int i = 0; i <= n; i++) {
+    		if(Math.pow(i, 3) == n) {
+    			cube = true;
+    		}
+    	}
+  	
+    	return cube;
     }
     
     public static int findBrokenEgg(List<String> eggs) {
-        return 0;
+    	int index = 0;
+    	for(int i = 0; i<eggs.size(); i++) {
+    		if(eggs.get(i).equals("cracked")) {
+    			index = i;
+    			i = eggs.size();
+    		}
+    	}
+    	
+        return index;
     }
+    public static int countPearls(List<Boolean> oysters) {
+    	int count = 0;
+    	for(int i = 0; i<oysters.size(); i++) {
+    		if(oysters.get(i)==true) {
+    			count++;
+    		}
+    	}
+    	
+    	return count;
+    }
+    public static double findTallest(List<Double> peeps) {
+    	double tallest = 0.0;
+    	for(int i=0; i<peeps.size(); i++) {
+    		if(peeps.get(i) > tallest) {
+    			tallest = peeps.get(i);
+    		}
+    	}
+    	
+    	return tallest;
+    }
+    
 }
--- src/_04_JavaClassSearchAndSort/_02_JavaClassSearchSort.java
+++ src/_04_JavaClassSearchAndSort/_02_JavaClassSearchSort.java
@@ -9,22 +9,71 @@ import java.util.List;
 public class _02_JavaClassSearchSort {
     
     public static int[] arraySort(int[] arr) {
-        
+    	 for (int i = 0; i < arr.length; i++) {
+             
+             // This for loop puts one value in the correct position
+             for (int k = 0; k < arr.length - 1; k++) {
+                 
+                 // If the current element is bigger than the next...
+                 if( arr[k] > arr[ k+1 ] ) {
+                     
+                     // Swap elements at k and k+1
+                     int temp = arr[k];
+                     arr[k] = arr[ k+1 ];
+                     arr[ k+1 ] = temp;
+                 }
+             }
+         }
+    	
+    	
         return null;
     }
     
     public static List<Double> listSort(List<Double> list){
-        
+    	Double[] arr = (Double[]) list.toArray();
+   	 for (int i = 0; i < arr.length; i++) {
+             
+             // This for loop puts one value in the correct position
+             for (int k = 0; k < arr.length - 1; k++) {
+                 
+                 // If the current element is bigger than the next...
+                 if( arr[k] > arr[ k+1 ] ) {
+                     
+                     // Swap elements at k and k+1
+                     double temp = arr[k];
+                     arr[k] = arr[ k+1 ];
+                     arr[ k+1 ] = temp;
+                 }
+             }
+         }
+    	
+    	
+    	
         return null;
     }
 
     public static Boolean arraySearch(char[] arr, char key) {
+        boolean present = false;
+        for(int i = 0; i< arr.length; i++) {
+        	if(arr[i] == key) {
+        		present = true;
+        	}
+        }
         
-        return null;
+        
+        
+        return present;
     }
     
     public static Boolean listSearch(List<Character> list, Character key) {
-        
-        return null;
+    	 boolean present = false;
+    	 for(int i = 0; i< list.size(); i++) {
+    		 if(list.get(i) == key)
+    			 present = true;
+    	 }
+    	 
+    	 
+    	 
+        return present;
     }
 }
